2dc2e7030e48024df9a02665e0f3296bf511c1cf
hemkdev <lucas.hemkemeier007@gmail.com>
2026-08-05 21:50:02 -0300

    Chore: Initial commit

---
+++ aula1/SistemaBiblioteca/BookModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    internal class BookModel
+    {
+        // Attributes - Properties
+        public string title;
+        public string isbm;
+        public string author;
+        public string genre;
+        public int pages;
+        public bool borrowed;
+
+
+        // Operations - Methods
+    }
+}
